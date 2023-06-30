38faca59083f620b2c35ef5214e346f66d45386f
Gokul <gokulp.21cse@kongu.edu>
2023-06-30 11:21:52 +0530

    First commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 require("dotenv").config();
 
 app.use(cors());
-
 app.use(express.json());
 
 // MongoDB Configuration
@@ -20,7 +19,37 @@ db.once('open', () => {
   console.log('Connected to the database');
 });
 
-// Schema and Model
+// Schema and Model for users
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/users', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username }).populate('buyers');
+
+    if (user) {
+      if (user.password === password) {
+        const buyerNames = user.buyers.map((buyer) => buyer.name);
+        res.status(200).json({ message: 'Login successful', buyerNames });
+      } else {
+        res.status(401).json({ error: 'Invalid password' });
+      }
+    } else {
+      res.status(401).json({ error: 'Invalid username' });
+    }
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ error: 'Failed to login' });
+  }
+});
+
+
 const saleSchema = new mongoose.Schema({
   buyer: String,
   quantity: Number,
@@ -47,25 +76,25 @@ app.get('/sales', async (req, res) => {
 });
 
 app.post('/sales', async (req, res) => {
-    try {
-      const { buyer, quantity, date, milkType } = req.body;
-  
-      const existingSale = await Sale.findOne({ buyer, date });
-      if (existingSale) {
-        existingSale.quantity = quantity; // Update the quantity of the existing sale
-        await existingSale.save();
-      } else {
-        const newSale = new Sale({ buyer, quantity, date, milkType });
-        await newSale.save(); // Save the new sale document to the database
-      }
-  
-      res.status(200).json({ message: 'Sale added successfully' });
-    } catch (error) {
-      console.error('Failed to add sale:', error);
-      res.status(500).json({ error: 'Failed to add sale' });
+  try {
+    const { buyer, quantity, date, milkType } = req.body;
+
+    const existingSale = await Sale.findOne({ buyer, date });
+    if (existingSale) {
+      existingSale.quantity = quantity; // Update the quantity of the existing sale
+      await existingSale.save();
+    } else {
+      const newSale = new Sale({ buyer, quantity, date, milkType });
+      await newSale.save(); // Save the new sale document to the database
     }
-  });
-  
+
+    res.status(200).json({ message: 'Sale added successfully' });
+  } catch (error) {
+    console.error('Failed to add sale:', error);
+    res.status(500).json({ error: 'Failed to add sale' });
+  }
+});
+
 app.put('/sales/:buyer/:date', async (req, res) => {
   try {
     const { buyer, date } = req.params;
